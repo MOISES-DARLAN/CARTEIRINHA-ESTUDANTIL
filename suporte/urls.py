44e606f0df5a0abb00577ef46e546a859cdd67d0
MOISES-DARLAN <moisesdarlan440@gmail.com>
2025-09-26 22:19:25 -0400

    fix(urls): Atualiza caminhos para registrar ticket e sucesso

--- suporte/urls.py
+++ suporte/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('suporte/', views.registrar_ticket, name='registrar_ticket'),
-    path('suporte/sucesso/', views.ticket_sucesso, name='ticket_sucesso'),
+    path('', views.registrar_ticket, name='registrar_ticket'),
+    path('sucesso/', views.ticket_sucesso, name='ticket_sucesso'),
 ]
